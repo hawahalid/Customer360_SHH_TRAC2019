b495d260fe44cfc18eb286cd494f89c63f70f559
Siti Hawa Halid <sitihawa.halid@delaware.pro>
2019-10-02 08:52:16 +0000

    commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 
 	return BaseController.extend("com.delaware.shh.trac2019.controller.App", {
 		onInit: function(){
-			 this.getModel("appView").setProperty("/layout", "OneColumn");
+			  this.getModel("appView").setProperty("/layout", "OneColumn");
 		}
 	});
 });
